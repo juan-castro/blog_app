c30066c681b430e7327e80385a791b0633a8e21c
Juan Castro <j.castro@tukaiz.com>
2013-02-21 17:49:31 -0600

    Project creation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+
+	end
+
+	def edit
+
+	end
+
+	def create
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = '28de7635ce653c7c6c1f3fa1ac0b66112d9e64e17ebb04b228418c347c9c81d013c6088ed39411916adc279a77eb97ffa218614e8c2bdd7412f3ca34f3e7eab4'
